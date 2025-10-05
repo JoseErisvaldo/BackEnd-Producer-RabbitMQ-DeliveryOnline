388b562f67cebf1ad340f4a8f41396730a9fae3b
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2025-10-05 19:46:14 -0300

    feat: enable CORS for specific origins in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ["https://delivery-online.netlify.app", "http://localhost:5173"],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3333);
 }
